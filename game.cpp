f58af643e315fdc0dcea95f4ac527bdc17dc9732
brentpearce <brentmp2@uwyo.edu>
2020-05-11 10:04:54 -0600

    Added decode funtction and removed bit rotation
    Bit rotation didn't seem to be working for(ie wasn't decoding properly)
    If time permits maybe we can add it back again.

--- game.cpp
+++ game.cpp
@@ -3,30 +3,37 @@
 
 using namespace std;
 
-void setUpGeme(string &name, string &characterClass);
+string* setUpGeme();
 
 void encode(string &toEncode);
+void decode(string &toEncode);
 
 int main(){
 
-  string name, characterClass;
 
-  int startingGold = 10, intell = 75, attack = 50, defense = 50, agility = 50;   
+  //int startingGold = 10, intell = 75, attack = 50, defense = 50, agility = 50;   
 
-  float health = 100.0, staminia = 99.9;
+  //float health = 100.0, staminia = 99.9;
 
 
-  setUpGeme(name, characterClass);  
+
+  //string *name = setUpGeme();  
 
   //encode somehow and save to a file..
-   
-
-
+  string s;
+  cout << "Enter a string\n";
+  cin >> s; 
+  cout << s << endl;
+  encode(s);
+  cout << s << endl;
+  cout << " " << endl;
+  decode(s);
+  cout << s << endl;
 
   return 0;
 }
 
-void setUpGeme(string &name, string &characterClass){
+  string* setUpGeme(){
 
   string name;
   cout << "Enter name (string)" << endl;
@@ -49,36 +56,49 @@ void setUpGeme(string &name, string &characterClass){
   cout << "Heatlth is 100.0" <<endl;
 
   cout << "Stamina is 99.9" << endl;
+
+  string *character = new string[2];
+  
+  character[0] = name;
+  character[1] = characterClass;
+
+  return character;
+
 }
 
 //thinking this could maybe be called inside a funtion backupToFIle 
 // or something...
 void encode(string &toEncode){
-  // key for xor and array for number of bits to rotate
-  const char key[3] = "42";
-  const long int rotate[4] = {1, 7, 3, 5};
-  const int char_bits = 8; //asuming using an ascii encoding.
+  // key for xor and 
+  const char XOR_KEY[4] = "~42";
       //do we want to add some indirection using pointers?
 
   // start by xor with the key
   for (int i = 0; i <toEncode.length(); i++)
   {
     //alternate between the first two chars 
-    char k = key[i%2];
-    if(k != toEncode[i]){
+    char k = XOR_KEY[i%3];
+    if(k != toEncode[i] && toEncode[i] != 0){
       //use the current key to conditionally xor the string
       toEncode[i] = toEncode[i] ^ k;
-      toEncode[i] = (toEncode[i] << rotate[i%4])| 
-                        (toEncode[i] >> (char_bits - rotate[i%4]));
     }
-    
-  
+  }
+}
 
+void decode(string &toDecode){
+
+  //Constants needed for decoding
+  const char XOR_KEY[4] = "~42";
+
+   for (int i = 0; i < toDecode.length(); i++) {
+       //get current key
+        char k = XOR_KEY[i%3];
+        if (toDecode[i] != 0 && toDecode[i] != k)
+        {
+          toDecode[i] = toDecode[i] ^ k;
+        }
+    
   }
   
   
-
-
-
-
 }
